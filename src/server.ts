63ca207c55a5d30906d90af8417b3d8ce19ea252
Simple Guruji <kumaramit957014@gmail.com>
2025-04-09 19:15:49 +0800

    test lint-staged 4

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,4 @@
 function welcome(name: string) {
-    const user = {
-        name,
-    }
-    const u1 = user.name
-    return `Hello ${u1}, Welcome to the world of programming`
+    console.log(`Hello ${name}, Welcome to the world of programming`)
 }
 welcome('Rohit Kumar')
